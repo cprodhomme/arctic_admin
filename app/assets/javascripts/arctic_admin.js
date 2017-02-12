dad71e0ebbba3ea6c4b779da7eda7967503d391d
Cle61 <prodhomme.clement@hotmail.fr>
2017-02-12 21:15:13 +0100

    upgrade width of the sidebar and the filter for large-screen

--- app/assets/javascripts/arctic_admin.js
+++ app/assets/javascripts/arctic_admin.js
@@ -1,14 +1,15 @@
 $(function() {
   $('#sidebar').click(function (e) {
-    var position = $(this).position()
+    var position = $(this).position();
+    var width = $(this).width();
     if (e.pageX < position.left) {
       if ($(this).css('right') == '0px') {
         $(this).animate({
-          right: "-=230"
+          right: "-="+width
         }, 600, function() {});
       } else {
         $(this).animate({
-          right: "+=230"
+          right: "+="+width
         }, 600, function() {});
       }
     }
