689a4d15ae81c99d77c9c8c7c42faaa7da898e90
Cle61 <prodhomme.clement@hotmail.fr>
2017-02-18 01:03:12 +0100

    close menu when click out of him

--- app/assets/javascripts/arctic_admin.js
+++ app/assets/javascripts/arctic_admin.js
@@ -9,39 +9,63 @@ $(function() {
       if ($(this).css('right') == '0px') {
         $(this).animate({
           right: "-="+width
-        }, 600, function() {});
+        }, 600, function() {
+          animationFilterDone = true;
+        });
       } else {
         $(this).animate({
           right: "+="+width
-        }, 600, function() {});
+        }, 600, function() {
+          animationFilterDone = true;
+        });
       }
     }
   });
 
   var animationDone = true;
   $('#title_bar').click(function (e) {
-    var position = $(this).position();
-    var tabs = $('#tabs');
-    var width = tabs.width() + 1;
+    if ($(window).width() < 960) {
+      var position = $(this).position();
+      var tabs = $('#tabs');
+      var width = tabs.width() + 1;
 
-    if (e.pageX < (position.left + 40)) {
-      if(animationDone == true) {
-        animationDone = false;
-        if (tabs.css('left') == '0px') {
-          tabs.animate({
-            left: "-="+width
-          }, 400, function() {
-            animationDone = true;
-          });
-        } else {
-          tabs.animate({
-            left: "+="+width
-          }, 400, function() {
-            animationDone = true;
-          });
+      if (e.pageX < (position.left + 40)) {
+        if(animationDone == true) {
+          animationDone = false;
+          if (tabs.css('left') == '0px') {
+            tabs.animate({
+              left: "-="+width
+            }, 400, function() {
+              animationDone = true;
+            });
+          } else {
+            tabs.animate({
+              left: "+="+width
+            }, 400, function() {
+              animationDone = true;
+            });
+          }
         }
       }
     }
   });
 
+  $('body').click(function (e) {
+    if ($(window).width() < 960) {
+      var tabs = $('#tabs');
+      var width = tabs.width() + 1;
+      if (tabs.css('left') == '0px') {
+        if (e.pageX > width && e.pageY > 50) {
+          if(animationDone == true) {
+            animationDone = false;
+            tabs.animate({
+              left: "-="+width
+            }, 400, function() {
+              animationDone = true;
+            });
+          }
+        }
+      }
+    }
+  });
 });
